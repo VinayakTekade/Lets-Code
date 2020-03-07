ca065d2f5b578d5f8abe81ceff6233434653d7c5
VinayakTekade <39454332+VinayakTekade@users.noreply.github.com>
2020-03-07 17:20:48 +0530

    Added content for popup
    Signed-off-by: VinayakTekade <39454332+VinayakTekade@users.noreply.github.com>

--- app.js
+++ app.js
@@ -11,4 +11,158 @@ $(window).on('load', function(){
      });
 
      
+    //Git
+     $("#git").click(function(){
+        $('.popup').show();
+        $('#git-popup').show();
+     });
+     $('.popupCloseButton').click(function(){
+        $('.popup').hide();
+        $('#git-popup').hide();
+     });
+
+     
+    //dsa
+     $("#dsa").click(function(){
+        $('.popup').show();
+        $('#dsa-popup').show();
+     });
+     $('.popupCloseButton').click(function(){
+        $('.popup').hide();
+        $('#dsa-popup').hide();
+     });
+
+     
+    //uiux
+     $("#uiux").click(function(){
+        $('.popup').show();
+        $('#uiux-popup').show();
+     });
+     $('.popupCloseButton').click(function(){
+        $('.popup').hide();
+        $('#uiux-popup').hide();
+     });
+
+     
+    //htmlcss
+     $("#htmlcss").click(function(){
+        $('.popup').show();
+        $('#htmlcss-popup').show();
+     });
+     $('.popupCloseButton').click(function(){
+        $('.popup').hide();
+        $('#htmlcss-popup').hide();
+     });
+
+     
+    //bootstrap
+     $("#bootstrap").click(function(){
+        $('.popup').show();
+        $('#bootstrap-popup').show();
+     });
+     $('.popupCloseButton').click(function(){
+        $('.popup').hide();
+        $('#bootstrap-popup').hide();
+     });
+
+     
+    //semantic
+     $("#semantic").click(function(){
+        $('.popup').show();
+        $('#semantic-popup').show();
+     });
+     $('.popupCloseButton').click(function(){
+        $('.popup').hide();
+        $('#semantic-popup').hide();
+     });
+
+     
+    //js
+     $("#js").click(function(){
+        $('.popup').show();
+        $('#js-popup').show();
+     });
+     $('.popupCloseButton').click(function(){
+        $('.popup').hide();
+        $('#js-popup').hide();
+     });
+
+     
+    //react
+     $("#react").click(function(){
+        $('.popup').show();
+        $('#react-popup').show();
+     });
+     $('.popupCloseButton').click(function(){
+        $('.popup').hide();
+        $('#react-popup').hide();
+     });
+
+     
+    //nodejs
+     $("#nodejs").click(function(){
+        $('.popup').show();
+        $('#nodejs-popup').show();
+     });
+     $('.popupCloseButton').click(function(){
+        $('.popup').hide();
+        $('#nodejs-popup').hide();
+     });
+
+     
+    //expressjs
+     $("#expressjs").click(function(){
+        $('.popup').show();
+        $('#expressjs-popup').show();
+     });
+     $('.popupCloseButton').click(function(){
+        $('.popup').hide();
+        $('#expressjs-popup').hide();
+     });
+
+     
+    //sql
+     $("#sql").click(function(){
+        $('.popup').show();
+        $('#sql-popup').show();
+     });
+     $('.popupCloseButton').click(function(){
+        $('.popup').hide();
+        $('#sql-popup').hide();
+     });
+
+     
+    //mongodb
+     $("#mongodb").click(function(){
+        $('.popup').show();
+        $('#mongodb-popup').show();
+     });
+     $('.popupCloseButton').click(function(){
+        $('.popup').hide();
+        $('#mongodb-popup').hide();
+     });
+
+     
+    //firebase
+     $("#firebase").click(function(){
+        $('.popup').show();
+        $('#firebase-popup').show();
+     });
+     $('.popupCloseButton').click(function(){
+        $('.popup').hide();
+        $('#firebase-popup').hide();
+     });
+
+     
+    //gcloud
+     $("#gcloud").click(function(){
+        $('.popup').show();
+        $('#gcloud-popup').show();
+     });
+     $('.popupCloseButton').click(function(){
+        $('.popup').hide();
+        $('#gcloud-popup').hide();
+     });
+
+     
 });
